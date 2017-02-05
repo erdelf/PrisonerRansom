716381ec48604ba8184e9d48f6296e62e0e2817b
erdelf <erdelf.productions@gmail.com>
2017-02-05 04:57:04 +0100

    Removed the detour of the dialog
    Added Harmony

--- Source/PrisonerRansom/ReplacementCode.cs
+++ Source/PrisonerRansom/ReplacementCode.cs
@@ -1,15 +1,11 @@
 ﻿using RimWorld;
-using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Verse;
-using HugsLib.GuiInject;
-using HugsLib.Source.Detour;
-using UnityEngine;
 using HugsLib;
 using HugsLib.Settings;
+using Harmony;
 
 namespace PrisonerRansom
 {
@@ -22,12 +18,13 @@ namespace PrisonerRansom
             // Thank god Zhentar
             LongEventHandler.QueueLongEvent(() =>
             {
-                Detour.TryDetourFromTo(typeof(FactionDialogMaker).GetMethod("FactionDialogFor"), typeof(ReplacementCode).GetMethod("_FactionDialogFor"));
-
                 ransomFactor = () => 2f;
                 ransomGoodwill = () => 5f;
                 ransomGoodwillFail = () => -10f;
                 ransomFailChance = () => 20f;
+
+                HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.prisoner_ransom");
+                harmony.Patch(typeof(FactionDialogMaker).GetMethod("FactionDialogFor"), null, new HarmonyMethod(typeof(ReplacementCode).GetMethod("FactionDialogForPostFix")));
 
                 try
                 {   //Need a wrapper method/lambda to be able to catch the TypeLoadException when HugsLib isn't present
@@ -60,110 +57,15 @@ namespace PrisonerRansom
         public static Func<float> ransomGoodwill;
         public static Func<float> ransomGoodwillFail;
         public static Func<float> ransomFailChance;
-
-        public static DiaNode _FactionDialogFor(Pawn negotiator, Faction faction)
-        {
-            Map map = negotiator.Map;
-            SetStaticField(typeof(FactionDialogMaker), "negotiator", negotiator);
-            SetStaticField(typeof(FactionDialogMaker), "faction", faction);
-            string text = (faction.leader != null) ? faction.leader.Name.ToStringFull : faction.Name;
-            if (faction.PlayerGoodwill < -70f)
-            {
-                SetStaticField(typeof(FactionDialogMaker), "root", new DiaNode("FactionGreetingHostile".Translate(new object[]
-                {
-                    text
-                })));
-            }
-            else if (faction.PlayerGoodwill < 40f)
-            {
-                string text2 = "FactionGreetingWary".Translate(new object[]
-                {
-                    text,
-                    negotiator.LabelShort
-                });
-                text2 = text2.AdjustedFor(negotiator);
-                SetStaticField(typeof(FactionDialogMaker), "root", new DiaNode(text2));
-                if(!FactionBaseUtility.IsPlayerAttackingAnyFactionBaseOf(faction))
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add(((DiaOption)InvokeMethod(typeof(FactionDialogMaker), "OfferGiftOption", null, map)));
-                if(!faction.HostileTo(Faction.OfPlayer) && negotiator.Spawned && map.IsPlayerHome)
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add(((DiaOption)InvokeMethod(typeof(FactionDialogMaker), "RequestTraderOption", null, null, new object[] { map, 600 })));
-            }
-            else
-            {
-                SetStaticField(typeof(FactionDialogMaker), "root", new DiaNode("FactionGreetingWarm".Translate(new object[]
-                {
-                    text,
-                    negotiator.LabelShort
-                })));
-                if(!FactionBaseUtility.IsPlayerAttackingAnyFactionBaseOf(faction))
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add((DiaOption)InvokeMethod(typeof(FactionDialogMaker), "OfferGiftOption", null, map));
-                if (!faction.HostileTo(Faction.OfPlayer) && negotiator.Spawned && map.IsPlayerHome)
-                {
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add((DiaOption)InvokeMethod(typeof(FactionDialogMaker), "RequestTraderOption", null, null, new object[] {map, 300}));
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add((DiaOption)InvokeMethod(typeof(FactionDialogMaker), "RequestMilitaryAidOption", null, map));
-                }
-            }
-            //Log.Message("test: " + faction.HostileTo(Faction.OfPlayer));
-            if(faction.HostileTo(Faction.OfPlayer))
-                ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add(RansomPrisoner(faction, negotiator, map));
-            if (Prefs.DevMode)
-            {
-                foreach (DiaOption current in (IEnumerable<DiaOption>)InvokeMethod(typeof(FactionDialogMaker), "DebugOptions"))
-                {
-                    ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add(current);
-                }
-            }
-            DiaOption diaOption = new DiaOption("(" + "Disconnect".Translate() + ")");
-            diaOption.resolveTree = true;
-            ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root")).options.Add(diaOption);
-            return ((DiaNode)GetStaticField(typeof(FactionDialogMaker), "root"));
-        }
-
-        internal static object GetStaticField(Type type, string fieldName)
-        {
-            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-                | BindingFlags.Static;
-            FieldInfo field = type.GetField(fieldName, bindFlags);
-            return field.GetValue(null);
-        }
-
-        internal static void SetStaticField(Type type, string fieldName, object value)
-        {
-            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-                | BindingFlags.Static;
-            FieldInfo field = type.GetField(fieldName, bindFlags);
-            field.SetValue(null, value);
-        }
-
-        internal static object InvokeMethod(Type type, string fieldName, object obj = null, object value = null, object[] values = null)
-        {
-            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-                | BindingFlags.Static;
-            MethodInfo method = type.GetMethod(fieldName, bindFlags);
-            return method.Invoke(obj, values != null ? values : value != null ? new object[] { value } : null);
-        }
         
-        
-        /*
-        [WindowInjection(typeof(Dialog_Negotiation), Mode = WindowInjectionManager.InjectMode.AfterContents)]
-        private static void RansomButton(Window window, Rect inRect)
+        public static void FactionDialogForPostFix(ref DiaNode __result, Pawn negotiator, Faction faction)
         {
-            Dialog_Negotiation dialog = window as Dialog_Negotiation;
-            Faction faction = ((Faction)typeof(FactionDialogMaker).GetField("faction", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            DiaNode root = ((DiaNode)typeof(FactionDialogMaker).GetField("root", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            Pawn negotiator = ((Pawn)typeof(FactionDialogMaker).GetField("negotiator", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-
-            if (dialog != null)
-                if (((DiaNode)typeof(Dialog_NodeTree).GetField("curNode", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(dialog)) == root)
-                    if (faction.HostileTo(Faction.OfPlayer))
-                        if (!root.options.Any((DiaOption dia) => ((string)typeof(DiaOption).GetField("text", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(dia)).Contains("ransom")))
-                        {
-                            root.options.Reverse();
-                            root.options.Add(RansomPrisoner(faction, negotiator, negotiator.Map));
-                            root.options.Reverse();
-                        }
+            if (faction.HostileTo(Faction.OfPlayer))
+            {
+                __result.options.Insert(0, RansomPrisoner(faction, negotiator, negotiator.Map));
+            }
         }
-        */
+
         private static DiaOption RansomPrisoner(Faction faction, Pawn negotiator, Map map)
         {
             IEnumerable<Pawn> prisoners = (from p in map.mapPawns.PrisonersOfColony where p.Faction == faction select p);
