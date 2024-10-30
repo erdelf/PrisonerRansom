1a0eb9e55cb4b3d34913f688077dc2889f0e3021
Simon Latusek <Simon.Latusek@gmail.com>
2024-10-30 17:29:20 +0100

    style

--- Source/PrisonerRansom/ReplacementCode.cs
+++ Source/PrisonerRansom/ReplacementCode.cs
@@ -15,8 +15,8 @@ namespace PrisonerRansom
     {
         public static RansomSettings settings;
 
-        public int ransomFactor       = 2;
-        public int adjustment         = 81;
+        public int ransomFactor = 2;
+        public int adjustment   = 81;
 
         public int ransomRaidDelay    = GenDate.TicksPerDay * 2;
         public int ransomFailCooldown = GenDate.TicksPerDay * 3;
@@ -25,17 +25,17 @@ namespace PrisonerRansom
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(value: ref this.ransomFactor, label: "ransomFactor", defaultValue: 2);
-            Scribe_Values.Look(value: ref this.adjustment,   label: "adjustment",   defaultValue: 81);
-            Scribe_Values.Look(value: ref this.ransomRaidDelay,     label: "ransomRaidDelay",     defaultValue: GenDate.TicksPerDay * 2);
-            Scribe_Values.Look(value: ref this.ransomFailCooldown, label: "ransomRaidFailCooldown", defaultValue: GenDate.TicksPerDay * 3);
+            Scribe_Values.Look(ref this.ransomFactor,       "ransomFactor",           2);
+            Scribe_Values.Look(ref this.adjustment,         "adjustment",             81);
+            Scribe_Values.Look(ref this.ransomRaidDelay,    "ransomRaidDelay",        GenDate.TicksPerDay * 2);
+            Scribe_Values.Look(ref this.ransomFailCooldown, "ransomRaidFailCooldown", GenDate.TicksPerDay * 3);
         }
 
         public static float MarketValue(Pawn p) => p.MarketValue * settings.ransomFactor;
 
         public static int MarketValuePercentage(Pawn p, float percentage) => Mathf.RoundToInt(MarketValue(p) * (percentage / 100f + 1));
 
-        public static float RansomChance(Pawn p, Pawn h, float percentage) => 
+        public static float RansomChance(Pawn p, Pawn h, float percentage) =>
             RansomChanceRaw(p.Faction.PlayerGoodwill, h.skills.GetSkill(SkillDefOf.Social).Level, percentage);
 
         public static float RansomChanceRaw(int factionGoodwill, int skillLevel, float percentage) =>
@@ -47,9 +47,9 @@ namespace PrisonerRansom
     {
         private readonly RansomSettings settings;
 
-        public PrisonerRansom(ModContentPack content) : base(content: content)
+        public PrisonerRansom(ModContentPack content) : base(content)
         {
-            this.settings = this.GetSettings<RansomSettings>();
+            this.settings           = this.GetSettings<RansomSettings>();
             RansomSettings.settings = this.settings;
         }
 
@@ -57,51 +57,55 @@ namespace PrisonerRansom
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            int ransomRaidDelay = Mathf.RoundToInt(this.settings.ransomRaidDelay / (float) GenDate.TicksPerHour);
+            int ransomRaidDelay    = Mathf.RoundToInt(this.settings.ransomRaidDelay    / (float)GenDate.TicksPerHour);
             int ransomFailCooldown = Mathf.RoundToInt(this.settings.ransomFailCooldown / (float)GenDate.TicksPerHour);
 
             Rect sliderSection = inRect.TopPart(0.7f);
 
-            this.settings.ransomFactor = (int) Widgets.HorizontalSlider(rect: sliderSection.TopHalf().TopHalf().BottomHalf(), value: this.settings.ransomFactor, min: 1f, max: 5f, middleAlignment: true, label: "SettingsRansomFactor".Translate(this.settings.ransomFactor), leftAlignedLabel: "1", rightAlignedLabel: "5");
-            ransomRaidDelay = (int) Widgets.HorizontalSlider(rect: sliderSection.TopHalf().BottomHalf().TopHalf(), value: ransomRaidDelay, min: 1f, max: 168f, middleAlignment: true, label: "SettingsRansomRaidDelay".Translate(this.settings.ransomRaidDelay.ToStringTicksToPeriod()), leftAlignedLabel: "1", rightAlignedLabel: "168");
-            ransomFailCooldown = (int) Widgets.HorizontalSlider(rect: sliderSection.BottomHalf().TopHalf().TopHalf(),value: ransomFailCooldown, min: ransomRaidDelay, max: 336f, middleAlignment: true, label: "SettingsRansomFailCooldown".Translate(this.settings.ransomFailCooldown.ToStringTicksToPeriod()), leftAlignedLabel: ransomRaidDelay.ToString(), rightAlignedLabel: "336");
-            this.settings.adjustment = (int) Widgets.HorizontalSlider(rect: sliderSection.BottomHalf().BottomHalf().TopHalf(), value: this.settings.adjustment, min: 40f, max: 95f, middleAlignment: true, label: "SettingsRansomAdjustment".Translate(this.settings.adjustment), leftAlignedLabel: "40", rightAlignedLabel: "95");
+            this.settings.ransomFactor = (int)Widgets.HorizontalSlider(sliderSection.TopHalf().TopHalf().BottomHalf(), this.settings.ransomFactor, 1f, 5f, true,
+                                                                       "SettingsRansomFactor".Translate(this.settings.ransomFactor), "1", "5");
+            ransomRaidDelay = (int)Widgets.HorizontalSlider(sliderSection.TopHalf().BottomHalf().TopHalf(), ransomRaidDelay, 1f, 168f, true,
+                                                            "SettingsRansomRaidDelay".Translate(this.settings.ransomRaidDelay.ToStringTicksToPeriod()), "1", "168");
+            ransomFailCooldown = (int)Widgets.HorizontalSlider(sliderSection.BottomHalf().TopHalf().TopHalf(), ransomFailCooldown, ransomRaidDelay, 336f, true,
+                                                               "SettingsRansomFailCooldown".Translate(this.settings.ransomFailCooldown.ToStringTicksToPeriod()), ransomRaidDelay.ToString(), "336");
+            this.settings.adjustment = (int)Widgets.HorizontalSlider(sliderSection.BottomHalf().BottomHalf().TopHalf(), this.settings.adjustment, 40f, 95f, true,
+                                                                     "SettingsRansomAdjustment".Translate(this.settings.adjustment), "40", "95");
 
-            this.settings.ransomRaidDelay = ransomRaidDelay * GenDate.TicksPerHour;
+            this.settings.ransomRaidDelay    = ransomRaidDelay    * GenDate.TicksPerHour;
             this.settings.ransomFailCooldown = ransomFailCooldown * GenDate.TicksPerHour;
 
             SimpleCurve curve = new SimpleCurve();
-            for (int i = -50; i <= 50; i++) 
+            for (int i = -50; i <= 50; i++)
                 curve.Add(i, RansomSettings.RansomChanceRaw(-75, 10, i) * 100);
 
             SimpleCurveDrawInfo drawInfo = new SimpleCurveDrawInfo()
                                            {
-                                               curve  = curve,
-                                               color  = Color.cyan,
+                                               curve       = curve,
+                                               color       = Color.cyan,
                                                valueFormat = "{0}%"
-            };
+                                           };
 
             SimpleCurveDrawer.DrawCurve(inRect.BottomPart(0.3f), drawInfo, new SimpleCurveDrawerStyle
-                                                                        {
-                                                                            DrawBackground           = false,
-                                                                            DrawBackgroundLines      = false,
-                                                                            DrawCurveMousePoint      = true,
-                                                                            DrawLegend               = true,
-                                                                            DrawMeasures             = true,
-                                                                            DrawPoints               = false,
-                                                                            FixedScale               = new Vector2(0, 100),
-                                                                            FixedSection             = new FloatRange(-50, 50),
-                                                                            LabelX                   = "Adj",
-                                                                            MeasureLabelsXCount      = 10,
-                                                                            MeasureLabelsYCount      = 10,
-                                                                            OnlyPositiveValues       = false,
-                                                                            PointsRemoveOptimization = false,
-                                                                            UseAntiAliasedLines      = true,
-                                                                            UseFixedScale            = true,
-                                                                            UseFixedSection          = true,
-                                                                            XIntegersOnly            = true,
-                                                                            YIntegersOnly            = true
-                                                                        });
+                                                                           {
+                                                                               DrawBackground           = false,
+                                                                               DrawBackgroundLines      = false,
+                                                                               DrawCurveMousePoint      = true,
+                                                                               DrawLegend               = true,
+                                                                               DrawMeasures             = true,
+                                                                               DrawPoints               = false,
+                                                                               FixedScale               = new Vector2(0, 100),
+                                                                               FixedSection             = new FloatRange(-50, 50),
+                                                                               LabelX                   = "Adj",
+                                                                               MeasureLabelsXCount      = 10,
+                                                                               MeasureLabelsYCount      = 10,
+                                                                               OnlyPositiveValues       = false,
+                                                                               PointsRemoveOptimization = false,
+                                                                               UseAntiAliasedLines      = true,
+                                                                               UseFixedScale            = true,
+                                                                               UseFixedSection          = true,
+                                                                               XIntegersOnly            = true,
+                                                                               YIntegersOnly            = true
+                                                                           });
         }
     }
 
@@ -111,48 +115,48 @@ namespace PrisonerRansom
 
         static ReplacementCode()
         {
-            Harmony harmony = new Harmony(id: "rimworld.erdelf.prisoner_ransom");
-            harmony.Patch(original: typeof(FactionDialogMaker).GetMethod(name: "FactionDialogFor"), prefix: null, postfix: new HarmonyMethod(typeof(ReplacementCode), nameof(FactionDialogForPostFix)));
+            Harmony harmony = new Harmony("rimworld.erdelf.prisoner_ransom");
+            harmony.Patch(typeof(FactionDialogMaker).GetMethod(nameof(FactionDialogMaker.FactionDialogFor)), postfix: new HarmonyMethod(typeof(ReplacementCode), nameof(FactionDialogForPostFix)));
         }
-        
+
         public static void FactionDialogForPostFix(ref DiaNode __result, Pawn negotiator, Faction faction)
         {
-            if (faction.HostileTo(other: Faction.OfPlayer))
+            if (faction.HostileTo(Faction.OfPlayer))
             {
                 DiaOption ransomPrisoner = RansomPrisoner(faction, negotiator, negotiator.Map, __result);
                 if (Page_PrisonerRansom.linkingBack)
                 {
                     Page_PrisonerRansom.linkingBack = false;
                     __result                        = ransomPrisoner.link;
-            }
+                }
                 else
                 {
                     __result.options.Insert(0, ransomPrisoner);
-        }
+                }
             }
         }
 
         private static DiaOption RansomPrisoner(Faction faction, Pawn negotiator, Map map, DiaNode original)
         {
             IEnumerable<Pawn> prisoners = map.mapPawns.PrisonersOfColony.Where(p => p.Faction == faction && p.CarriedBy == null).ToArray();
-            DiaOption dia = new DiaOption(text: "DialogRansomDemand".Translate());
+            DiaOption         dia       = new DiaOption("DialogRansomDemand".Translate());
             if (!prisoners.Any())
-                dia.Disable(newDisabledReason: "DialogRansomNoPrisoners".Translate());
-            DiaNode diaNode = new DiaNode(text: "DialogRansomPrisonerList".Translate());
+                dia.Disable("DialogRansomNoPrisoners".Translate());
+            DiaNode diaNode = new DiaNode("DialogRansomPrisonerList".Translate());
             foreach (Pawn p in prisoners)
             {
-                int value = Mathf.RoundToInt(f: RansomSettings.MarketValue(p));
+                int value = Mathf.RoundToInt(RansomSettings.MarketValue(p));
 
-                DiaOption diaOption = new DiaOption(text: p.Name.ToStringFull + " (" + value + ")")
+                DiaOption diaOption = new DiaOption(p.Name.ToStringFull + " (" + value + ")")
                                       {
                                           resolveTree = true,
-                                          action = () => Find.WindowStack.Add(new Page_PrisonerRansom(p, negotiator))
+                                          action      = () => Find.WindowStack.Add(new Page_PrisonerRansom(p, negotiator))
                                       };
-                
-                diaNode.options.Add(item: diaOption);
+
+                diaNode.options.Add(diaOption);
             }
 
-            diaNode.options.Add(new DiaOption("GoBack".Translate()) { link = original});
+            diaNode.options.Add(new DiaOption("GoBack".Translate()) { link = original });
             dia.link = diaNode;
             return dia;
         }
@@ -162,14 +166,14 @@ namespace PrisonerRansom
     {
         internal static bool linkingBack;
 
-        private readonly Pawn prisoner;
-        private readonly Pawn handler;
-        private float percentage;
+        private readonly Pawn  prisoner;
+        private readonly Pawn  handler;
+        private          float percentage;
 
         public Page_PrisonerRansom(Pawn prisoner, Pawn handler)
         {
             this.prisoner = prisoner;
-            this.handler = handler;
+            this.handler  = handler;
         }
 
         public override Vector2 InitialSize => new Vector2(400, 300);
@@ -195,13 +199,18 @@ namespace PrisonerRansom
             listingStandard.Gap();
             listingStandard.Gap();
 
-            listingStandard.Label(label: $"RansomDemandAmount".Translate(RansomSettings.MarketValuePercentage(this.prisoner, this.percentage).ToString(CultureInfo.CurrentCulture), ThingDefOf.Silver.LabelCap));
-            
+            listingStandard.Label($"RansomDemandAmount".Translate(RansomSettings.MarketValuePercentage(this.prisoner, this.percentage).ToString(CultureInfo.CurrentCulture),
+                                                                  ThingDefOf.Silver.LabelCap));
+
             this.percentage = listingStandard.Slider(this.percentage, -50f, 50f);
             listingStandard.Gap();
-            listingStandard.Label(label: $"RansomDemandChance".Translate(Mathf.RoundToInt(RansomSettings.RansomChance(this.prisoner, this.handler, this.percentage) * 100).ToString(CultureInfo.CurrentCulture)));
+
             float ransomChance = RansomSettings.RansomChance(this.prisoner, this.handler, this.percentage);
+
             listingStandard.Label($"RansomDemandChance".Translate(Mathf.RoundToInt(ransomChance * 100)
+                                                                       .ToString(CultureInfo.CurrentCulture)));
+
+            listingStandard.Gap();
             if (listingStandard.ButtonText("RansomSendOffer".Translate()))
             {
                 linkingBack = true;
@@ -210,14 +219,14 @@ namespace PrisonerRansom
                 Faction faction = this.prisoner.Faction;
                 if (Rand.Value < ransomChance)
                 {
-                    Messages.Message(text: "RansomFactionDeliveredMessage".Translate(), def: MessageTypeDefOf.PositiveEvent);
-                    Thing silver = ThingMaker.MakeThing(def: ThingDefOf.Silver);
+                    Messages.Message("RansomFactionDeliveredMessage".Translate(), MessageTypeDefOf.PositiveEvent);
+                    Thing silver = ThingMaker.MakeThing(ThingDefOf.Silver);
                     silver.stackCount = RansomSettings.MarketValuePercentage(this.prisoner, this.percentage);
-                    TradeUtility.SpawnDropPod(dropSpot: DropCellFinder.TradeDropSpot(map: this.prisoner.Map), map: this.prisoner.Map, t: silver);
+                    TradeUtility.SpawnDropPod(DropCellFinder.TradeDropSpot(this.prisoner.Map), this.prisoner.Map, silver);
 
                     if (this.prisoner.Spawned)
                     {
-                        GenGuest.PrisonerRelease(p: this.prisoner);
+                        GenGuest.PrisonerRelease(this.prisoner);
                         this.prisoner.DeSpawn();
                     }
 
@@ -233,11 +242,13 @@ namespace PrisonerRansom
 
                     IncidentParms incidentParms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, this.prisoner.Map);
 
-                    incidentParms.faction = faction;
+                    incidentParms.faction      = faction;
                     incidentParms.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
 
-                    Find.Storyteller.incidentQueue.Add(IncidentDefOf.RaidEnemy, Find.TickManager.TicksGame + RansomSettings.settings.ransomRaidDelay, incidentParms, RansomSettings.settings.ransomRaidDelay);
+                    Find.Storyteller.incidentQueue.Add(IncidentDefOf.RaidEnemy, Find.TickManager.TicksGame + RansomSettings.settings.ransomRaidDelay, incidentParms,
+                                                       RansomSettings.settings.ransomRaidDelay);
                 }
+
                 this.Close();
             }
 
@@ -246,7 +257,7 @@ namespace PrisonerRansom
                 linkingBack = true;
                 this.prisoner.Faction.TryOpenComms(this.handler);
                 this.Close();
-        }
+            }
 
             listingStandard.End();
         }
